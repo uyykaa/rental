cf6de7b482b71e86e5295e3ffd1f879f22062151
kholisardi <kholisardiyanto@gmail.com>
2024-06-23 19:51:11 +0700

    sewa lagi

--- sewa-kendaraan.php
+++ sewa-kendaraan.php
@@ -109,7 +109,7 @@ while ($customer = mysqli_fetch_assoc($customers_query)) {
                       <div class="modal-content">
                         <div class="modal-header">
                           <h4 class="modal-title">Ubah Data Sewa</h4>
-                          <button type="button" class="close" data-dismiss="modal">&times;"></button>
+                          <button type="button" class="close" data-dismiss="modal">&times;</button>
                         </div>
                         <div class="modal-body">
                           <form role="form" action="proses-edit-sewa-kendaraan.php" method="POST">
