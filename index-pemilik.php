4b8e3202986a56d6dab94e7b0e40fa46e9eca30f
uyykaa <reza.widya@students.utdi.ac.id>
2024-07-30 22:00:09 +0700

    Update index-pemilik.php

--- index-pemilik.php
+++ index-pemilik.php
@@ -16,7 +16,6 @@ if ($_SESSION['role_id'] != '2') {
   <meta name="author" content="">
   <link rel="shortcut icon" href="img/logo.jpg">
   <title>SIA KAS GC PERSADA </title>
-
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
   <link href="css/sb-admin-2.min.css" rel="stylesheet">
@@ -73,7 +72,7 @@ if ($_SESSION['role_id'] != '2') {
       </button>
 
       <!-- Topbar Search -->
-      <h3> Selamat Datang di Web <b>SIA Kas GC Persada </b></h3>
+      <h1>Selamat Datang, <?= $_SESSION['nama'] ?></h1>
 
       <?php require 'user.php'; ?>
     </nav>
